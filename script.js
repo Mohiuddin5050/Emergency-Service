7c9a247467c52d8625cbce50e31bac9d03ce3dc9
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-08-29 03:37:15 +0600

    Call Buttons and  Call History Section js done

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 
+
 // Heart icon
 const heartIcons = document.getElementsByClassName("button-1")
 
-for(const heartIcon of heartIcons){
-    heartIcon.addEventListener("click", function(){
-        const totalHeart=parseInt(document.getElementById("heart-count").innerText)
-        const totalHeartCount=totalHeart+1
+for (const heartIcon of heartIcons) {
+    heartIcon.addEventListener("click", function () {
+        const totalHeart = parseInt(document.getElementById("heart-count").innerText)
+        const totalHeartCount = totalHeart + 1
         document.getElementById("heart-count").innerText = totalHeartCount
     })
 }
 
-// Call button
+function getElement(id) {
+    const element = document.getElementById(id);
+    return element;
+}
+const cardButton = document.getElementsByClassName("call-btn-1")
+for (const card of cardButton) {
+    card.addEventListener("click", function () {
+        const totalCoin = parseInt(document.getElementById("coin-count").innerText)
+        if (totalCoin < 20) {
+            alert("You don't have enough coins, you need at least 20 coins to make a call.")
+            return
+        }
+        
+        const totalCoinCount = totalCoin - 20
+        document.getElementById("coin-count").innerText = totalCoinCount
+        document.getElementById("block").style.display = "block"
+        const serviceName = card.parentNode.parentNode.children[1].children[0].innerText;
+        const serviceNumber = card.parentNode.parentNode.children[1].children[2].innerText;
+        const time = new Date().toLocaleTimeString()
+        alert(`calling ${serviceName} ${serviceNumber}...`)
+        const container = getElement("container")
+        const div = document.createElement("div")
+        div.innerHTML = `<div class="flex justify-between items-center gap-2 pt-3">
+                        <div>
+                            <h2 class="font-bold">${serviceName}</h2>
+                            <p>${serviceNumber}</p>
+                        </div>
+                        <div>
+                            <p>${time}</p>
+                        </div>
+                    </div>`
+        container.appendChild(div);
 
+    })
 
+}
+document.getElementById("clear-btn").addEventListener("click", function () {
+    document.getElementById("block").style.display = "none"
+    historyData.length = 0
+    document.getElementById("container").innerHTML = ""
+})
